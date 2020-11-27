88206052e20fc841bb5e6fb4bd94956ef80cdb41
srj2001 <73327225+srj2001@users.noreply.github.com>
2020-11-27 23:26:55 +0530

    Update login.java

--- BloodBank Management/src/login.java
+++ BloodBank Management/src/login.java
@@ -1,24 +1,25 @@
 
-import java.awt.Color;
-import java.awt.Font;
+import java.awt.Color;  // abstract window toolkit - graphical UI to applications - creates static components
+import java.awt.Font; 
 import java.awt.LayoutManager;
-import java.awt.event.*;
+import java.awt.event.*; // used to provide the life to static components. 
 import java.sql.*;
-import javax.swing.*;
+import javax.swing.*; // enhance graphical interface
 import java.time.format.DateTimeFormatter;  
 import java.time.LocalDateTime; 
 
-public class login extends JFrame implements ActionListener
+// jab bhi koi class kisi interface ko implement karti he to uss interfcace ka koi bhi method ko uss class me overrride karwana padta he
+public class login extends JFrame implements ActionListener // jo bhi method Jframe class me he wo login class bhi use kar sktra he - inheritance
 {
 	/*
 	  JFrame is used when working with Swing.
 	  JFrame creates the view and ActionListener is used to create the application responsive, i.e., control what happens when user clicks on a button etc.
 	  To implement the ActionListener, we need to override the actionPerformed method.
 	*/
-	JLabel JL1,JL3,JL4; //These are basically the text views
+	JLabel JL1,JL3,JL4; //These are basically the text views - name,password
 	JComboBox JCB1; //dropdown menu option
 	JTextField JT1,JT2; //These are the places where user enters the data (edit texts)
-	JPasswordField JP1; //The text field in which the input method is of type password.
+	JPasswordField JP1; //The text field in which the input method is of type password. ( dot dot type)
 	JButton JB1,JB2,JB3,JB4,JB5; //The buttons
 	JComboBox JR1;
 	/* JB1 Login
@@ -29,8 +30,8 @@ public class login extends JFrame implements ActionListener
 	login()
 	{ //in this constructor, we will define the global variables
 		
-		setTitle("Blood Bank Management System");
-		// This is the title which appears at the top of the application
+		setTitle("Blood Bank Management System"); // method or function
+		// This is the title which appears at the top of the application 
 		
 		
 		
@@ -46,7 +47,7 @@ public class login extends JFrame implements ActionListener
 		JL3=new JLabel("Password:");
 		JL3.setFont(new Font("Osward",Font.BOLD,20));
 
-		JL4=new JLabel("User Type:");
+		JL4=new JLabel("User Type:"); // user type - donor or staff
 		JL4.setFont(new Font("Osward",Font.BOLD,15));
 
 		//Text and password
@@ -54,8 +55,8 @@ public class login extends JFrame implements ActionListener
 		
 		JR1=new JComboBox(user);
 		
-		JT1=new JTextField(15); //Here, 15 means that the text entry can be of length 15(i.e., 15 columns)
-		JP1=new JPasswordField(8);
+		JT1=new JTextField(15); //Here, 15 means that the text entry can be of length 15(i.e., 15 columns) // text can be id or password
+		JP1=new JPasswordField(8); 
 		
 		//Buttons
 		JB1=new JButton("Login"); //Login is the argument. It appears on the button 
@@ -77,7 +78,7 @@ public class login extends JFrame implements ActionListener
 		//This is a method of jawa.awt.Component class, used to set the position and size of a component.
 		
 		JL1.setBounds(175,0,450,200);//applied to a label
-		add(JL1);//to show it in the frame
+		add(JL1);//to show it in the frame // baxxe me daalne ke liye
 		
 		JCB1.setBounds(125,240,150,25); //to avoid overlapping we change the x,y coordinates for each
 		add(JCB1);
@@ -114,7 +115,7 @@ public class login extends JFrame implements ActionListener
 		JB4.setBounds(500,450,100,30);
 		add(JB4);
 		
-		JB1.addActionListener(this); //what happens when user clicks on b1 button
+		JB1.addActionListener(this); //jaise hi button par click hoga wo ek overridden method(actionlistner waala) me jaayega// what happens when user clicks on b1 button 
 		JB2.addActionListener(this);
 		JB3.addActionListener(this);
 		JB4.addActionListener(this);
@@ -128,10 +129,11 @@ public class login extends JFrame implements ActionListener
 		
 	}
 	@Override
-	public void actionPerformed(ActionEvent a) //a is the object of ActionEvent class
+	public void actionPerformed(ActionEvent a) //a is the object of ActionEvent class 
+	// this is method in actionListener interface
 	{
-		//surrounded with try catch because here we are dealing with an external file(i.e. the database)
-		try
+		//surrounded with try catch because here we are dealing with an external file(i.e. the database)SQL
+		try // it first goes in try 
 		{
 				connect c=new connect();//creating the object of connection class so that we can use our database
 				String user=JT1.getText();//ID or phonenumber
@@ -139,12 +141,13 @@ public class login extends JFrame implements ActionListener
 				//getText() is used to retrieve the data from any textfield etc.
 				String query; //the mysql query
 				String q2;
-				if(a.getSource()==JB1)//if button jb1 is clicked
+				if(a.getSource()==JB1)//if button jb1 is clicked ( lpogin waala button)
 				{
 					if(JCB1.getSelectedItem().equals("ID")) //if the user is logging in using his user id
 					{
 						query="select*from user where UserID='"+user+"'and password='"+password+"'";
-						ResultSet r=c.s.executeQuery(query); //it stores the result of executing an SQL query  
+						ResultSet r=c.s.executeQuery(query); // ResultSet is an interface to represent a database table 
+						// r is a reference variable  of resultSet interface which holds the data retrieved from a table after an SQL query is executed.
 						if(r.next())
 						//to retrieve the data from the database we use next().
 						//it jumps column by column till it reaches the last column
@@ -154,19 +157,19 @@ public class login extends JFrame implements ActionListener
 							ResultSet rs=c.s.executeQuery(q2);
 							if(rs.next())
 							{
-								if(!(rs.getString("post").equals((String)JR1.getSelectedItem())))
+								if(!(rs.getString("post").equals((String)JR1.getSelectedItem()))) 
 								{
 									JOptionPane.showMessageDialog(null,"Check your User Type");
 								}
 								else if(JR1.getSelectedItem().equals("Donor"))
 								{
-									setVisible(false);//making the current frame invisible
+									setVisible(false);//making the current frame invisible // true hua to bg me chalta rahega
 									new home_donor().setVisible(true); //opens the home_donor class
 								}
 								else if(JR1.getSelectedItem().equals("Staff"))
 								{
 									setVisible(false);
-									new staff().setVisible(true); //opens the home_donor class
+									new staff().setVisible(true); //opens the staff class
 								}
 							}
 						}
@@ -208,7 +211,7 @@ public class login extends JFrame implements ActionListener
 						}
 					}
 				}
-				else if(a.getSource()==JB2)
+				else if(a.getSource()==JB2)  // forgot passsword
 				{//when jb2 button is clicked
 					JTextField JT2;
 					JT2=new JTextField(10);
@@ -216,7 +219,7 @@ public class login extends JFrame implements ActionListener
 					String phonenumber=JOptionPane.showInputDialog(JT2,"Enter Phone Number");
 					String fp="select * from user where UserID="+"'"+ID+"'"+"AND phonenumber=" +"'"+phonenumber+"'";
 					ResultSet q=c.s.executeQuery(fp);
-					if(q.next())
+					if(q.next()) // check if exists
 					{
 						String x=q.getString("password");
 						JOptionPane.showMessageDialog(null,"Password is "+x);
@@ -227,19 +230,25 @@ public class login extends JFrame implements ActionListener
 					}
 					//System.out.println(s);
 				}
-				else if(a.getSource()==JB3)
+				else if(a.getSource()==JB3) // signup
 				{
 					new signup().setVisible(true);
 					setVisible(false);
 				}
-				else if(a.getSource()==JB4)
+				else if(a.getSource()==JB4) // clear (txt field empty)
 				{
-					JT1.setText("");
+					JT1.setText(""); // null kardiya empty the txt fields
 					JP1.setText("");
 				}
-		}catch(Exception e)
+		// catch me tabhi aayga jab try me error aayega
+		}catch(Exception e) 
+		// exception is the highest class in hierarchy order// badi parent class
+		
+		// contains or handle all the exceptions like sql,i/o,arithmetic etc
+		
+		// exception can be sql or i/o exception.
 		{
-			e.printStackTrace();
+			e.printStackTrace(); // helps u find in which class or in which line there is an error
 			//System.out.println("Sorry for inconvenience");
 			JOptionPane.showMessageDialog(null,"Sorry for inconvenience");
 		}
